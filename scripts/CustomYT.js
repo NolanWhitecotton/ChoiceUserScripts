be2b1a1ac9647124540213ab772863e3101c9b6e
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2021-06-21 23:27:33 -0500

    CustomYT: Made indentation consistent

--- scripts/CustomYT.js
+++ scripts/CustomYT.js
@@ -33,18 +33,18 @@ var menuItemClassName = "ytp-menuitem-content";
 // ===== code =====
 //adds amt to the video speed
 function changeSpeed(amt){
-  document.getElementsByTagName(VideoTagName)[0].playbackRate = document.getElementsByTagName(VideoTagName)[0].playbackRate + amt;
+	document.getElementsByTagName(VideoTagName)[0].playbackRate = document.getElementsByTagName(VideoTagName)[0].playbackRate + amt;
 }
 
 //update the playback speed notifier in the video play bar
 function updateSpeed(){
-  var labels = document.getElementsByClassName(menuLabelClassName);
-  for(var i=0;i<labels.length;i++){
-    if(labels[i].innerHTML==playbackSpeedMenuTitle){
-      var speed = document.getElementsByTagName(VideoTagName)[0].playbackRate;
-      document.getElementsByClassName(menuItemClassName)[i].innerHTML = (speed==1 ? DefaultPlaybackSpeedName : speed);
-    }
-  }
+	var labels = document.getElementsByClassName(menuLabelClassName);
+	for(var i=0;i<labels.length;i++){
+		if(labels[i].innerHTML==playbackSpeedMenuTitle){
+			var speed = document.getElementsByTagName(VideoTagName)[0].playbackRate;
+			document.getElementsByClassName(menuItemClassName)[i].innerHTML = (speed==1 ? DefaultPlaybackSpeedName : speed);
+		}
+	}
 }
 
 //update hidden/unhidden video state 
@@ -52,44 +52,44 @@ var watchedAreHidden = false; //whether the videos are hidden
 var hiddenVideos = []; //list of currently hidden videos
 setInterval(updateHidden, hideCheckInterval);
 function updateHidden(){
-  if(watchedAreHidden){
-    //hide videos
-    var bars = document.getElementsByClassName(progressBarClassName);
-    for(var i=1;i<bars.length;i++){
-      var curBar = bars[i];
-      //get the parent until it is a full video
-      while(curBar.className!=fullVideoClassName){
-        curBar = curBar.parentElement;
-      }
-      //hide and add to hiddenVideoss
-      curBar.hidden=true;
-      hiddenVideos.push(curBar);
-    }
-  }else{
-    //unhide videos
-    for(var i=hiddenVideos.length-1;i>=0;i--){
-      hiddenVideos[i].hidden=false;
-    }
-  }
+	if(watchedAreHidden){
+		//hide videos
+		var bars = document.getElementsByClassName(progressBarClassName);
+		for(var i=1;i<bars.length;i++){
+			var curBar = bars[i];
+			//get the parent until it is a full video
+			while(curBar.className!=fullVideoClassName){
+				curBar = curBar.parentElement;
+			}
+			//hide and add to hiddenVideoss
+			curBar.hidden=true;
+			hiddenVideos.push(curBar);
+		}
+	}else{
+		//unhide videos
+		for(var i=hiddenVideos.length-1;i>=0;i--){
+			hiddenVideos[i].hidden=false;
+		}
+	}
 }
 
 //handle keypresses
 document.addEventListener("keypress", function(event) {
-  //toggle hide
-  if (event.code == hideKey) {
-    watchedAreHidden = !watchedAreHidden;
-    updateHidden();
-  }
-  console.log(event.code)
+	//toggle hide
+	if (event.code == hideKey) {
+		watchedAreHidden = !watchedAreHidden;
+		updateHidden();
+	}
+	console.log(event.code)
 
-  //inc speed
-  if (event.code == incSpeedKey) {
-    changeSpeed(speedChangeAmmt);
-    updateSpeed();
-  }
-  //dec speed
-  if (event.code == decSpeedKey) {
-    changeSpeed(speedChangeAmmt*-1);
-    updateSpeed();
-  }
+	//inc speed
+	if (event.code == incSpeedKey) {
+		changeSpeed(speedChangeAmmt);
+		updateSpeed();
+	}
+	//dec speed
+	if (event.code == decSpeedKey) {
+		changeSpeed(speedChangeAmmt*-1);
+		updateSpeed();
+	}
 })
