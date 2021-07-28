49ab43932fff2476614063e5fcb6390506c3336f
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2021-07-28 17:20:33 -0500

    BetterDates: Update the time every 3 seconds
    This is to let the whole page load and to fix switching leaderboards not causing it to update.

--- scripts/BetterDates.js
+++ scripts/BetterDates.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     better dates
-// @version  1
+// @version  1.1
 // @grant    none
 // @match 	 https://www.speedrun.com/*
 // ==/UserScript==
@@ -8,7 +8,15 @@
 //replace the viewable date, with its title, the more accurate version
 //this is useful for archiving purposes, as you can have a more exact
 //timeframe as opposed to a date relative to current
-var dates = document.getElementsByClassName("short circa");
-for(var i=0;i<dates.length;i++){
-  dates[i].innerHTML = dates[i].title; 
+
+//replaces all the dates on the page with the exact date and time
+function updateDates(){
+	let dates = document.getElementsByClassName("short circa");
+	for(let i=0;i<dates.length;i++){
+		dates[i].innerHTML = dates[i].title; 
+	}
 }
+
+sleep(2000).then(() => {
+	updateDates();
+})
