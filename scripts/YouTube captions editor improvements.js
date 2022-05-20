81d5403a0a56763d427be7e5e5b1389c6fa4f173
Nolan Whitecotton <nolanwhitecotton@gmail.com>
2022-05-20 15:24:18 -0500

    YTCEI: Simplified the ways that the code works
    -Made the ctrl+s event listener more straight forward
    -Added a function to inject the CSS instead of doing it in place

--- scripts/YouTube captions editor improvements.js
+++ scripts/YouTube captions editor improvements.js
@@ -5,43 +5,15 @@
 // @description  Fixes some annoying things with the new YouTube captions editor.
 // @author       Nolan Whitecotton
 // @match        https://studio.youtube.com/video/*/translations
+// @match        https://studio.youtube.com/video/EPIW3DeZViw/translations
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    //ctrl+s to save captions draft
-    let keysPressed = {};
-
-    document.addEventListener('keyup', (event) => {
-        delete this.keysPressed[event.key];
-    });
-
-    document.addEventListener('keydown', (event) => {
-        keysPressed[event.key] = true;
-
-        if (keysPressed.Control && event.key == 's') {
-            event.preventDefault();
-            document.getElementById("save-draft-button").click();
-        }
-    });
-
-    //display highlighted region and double drag size
-    var css = `
-ytve-captions-marker[marker-type=captions-right] #touch-area.ytve-captions-marker {
-	left: -10px;
-}
-
-#touch-area.ytve-captions-marker {
-	width: 10px;
-}
-
-.ytve-captions-marker:hover  {
-  background-color: #f2ff0073;
-}
-`,
+//injects css as a string to the page
+// css injection javascript source: https://stackoverflow.com/a/524721/9826113
+function injectCSS(cssToInject){
+    let css = cssToInject,
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
 
@@ -54,6 +26,33 @@ ytve-captions-marker[marker-type=captions-right] #touch-area.ytve-captions-marke
     } else {
         style.appendChild(document.createTextNode(css));
     }
+}
+
+(function() {
+    'use strict';
+
+    //ctrl+s to save captions draft
+    document.addEventListener('keydown', function(event) {
+        if (event.code == "KeyS" && event.ctrlKey == true) {
+            event.preventDefault();
+            document.getElementById("save-draft-button").click();
+        }
+    });
+
+    //display highlighted region and double drag size
+    injectCSS(`
+ytve-captions-marker[marker-type=captions-right] #touch-area.ytve-captions-marker {
+	left: -10px;
+}
+
+#touch-area.ytve-captions-marker {
+	width: 10px;
+}
+
+.ytve-captions-marker:hover  {
+  background-color: #f2ff0073;
+}
+`);
 
 
 })();
